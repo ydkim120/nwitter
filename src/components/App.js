612c361173b312d782802438cb257aa4d67b941c
dam <yedam.kim@growthsoft.co.kr>
2022-09-17 16:46:57 +0900

    feat - 메세지 crud 초기 구현 (snapshot 이벤트 이용한 실시간 db 연동)

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,25 @@ import { authService } from 'fbase'
 function App() {
   const [init, setInit] = useState(false)
   const [isLoggedIn, setIsLoggedIn] = useState(false) // 로그인 유무
+  const [userObj, setUserObj] = useState(null)
 
   useEffect(() => { 
     authService.onAuthStateChanged((user) => { // 계정에 변화가 있을 때 실행되는 메서드
-      setIsLoggedIn(!!user)
-      setInit(true)
+      setInit(true) // 페이지 시작
+      setUserObj(user) // 유저 정보
+      setIsLoggedIn(!!user) // 로그인 유무
     })
   }, [])
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initializing...'}
+      {init ? (
+        <AppRouter 
+          isLoggedIn={isLoggedIn}
+          userObj={userObj}
+        />
+      ) : (
+        'Initializing...'
+      )}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,70 @@
-import React from 'react'
+import { dbService } from 'fbase'
+import React, { useEffect, useState } from 'react'
 
-const Home = () => <span>Home</span>
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState('')
+  const [nweetList, setNweetList] = useState([])
+
+  // 요런 방식은 좀 구식
+  // const getNweets = async () => {
+  //   const response = await dbService.collection('nweets').get()
+  //   response.forEach(doc => {
+  //     const nweetObject = {
+  //       ...doc.data(),
+  //       id: doc.id
+  //     }
+  //     setNweetList((prev) => [nweetObject, ...prev]) // set 함수의 경우 함수로 작성할 수 있으며, 이전 값에 접근할 수 있다.
+  //   })
+  // }
+  useEffect(() => {
+    // getNweets()
+    dbService.collection('nweets').onSnapshot(snapshot => {
+      const nweetArr = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      setNweetList(nweetArr)
+    })
+  }, [])
+  const onSubmit = async (event) => {
+    event.preventDefault()
+    await dbService.collection('nweets').add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj?.uid
+    })
+    setNweet('')
+  }
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event
+    if (value) setNweet(value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="test"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          value={nweet}
+          onChange={onChange}
+        />
+        <input
+          type="submit" 
+          value="Nweet" 
+        />
+      </form>
+      <div>
+        {nweetList.map(item => (
+          <div key={item.id}>
+            <h4>{item.text}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 export default Home
