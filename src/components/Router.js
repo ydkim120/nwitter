5908f353073bfd3d4a3e6ccfb1a1397df54297e3
dam <yedam.kim@growthsoft.co.kr>
2022-09-17 14:17:57 +0900

    feat- 로그아웃 로직 추가, 로그아웃 시 Home으로 redirect

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,22 +1,33 @@
 import React, { useState } from 'react'
-import { HashRouter as Router, Route, Switch  } from 'react-router-dom'
-import Auth from '../routes/Auth'
-import Home from '../routes/Home'
+import { HashRouter as Router, Redirect, Route, Switch  } from 'react-router-dom'
+import Auth from 'routes/Auth'
+import Home from 'routes/Home'
+import Profile from 'routes/Profile'
+import Navigation from 'components/Navigation'
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            {/* 404 페이지 같은 경우 이런 식으로 Redirect를 사용할 수 있음 */}
+            {/* <Redirect from="*" to="/" />  */}
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            {/* <Redirect from="*" to="/" /> */}
+          </>
         )}
       </Switch>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,21 @@
+import { authService } from 'fbase'
 import React from 'react'
+import { useHistory } from 'react-router'
 
-export default () => <span>Profile</span>;
+export default () => {
+  const history = useHistory()
+
+  /**
+   * 로그아웃 이벤트
+   * Home으로 라우팅
+   */
+  const onLogOutClick = () => {
+    authService.signOut()
+    history.push('/')
+  }
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  )
+}
