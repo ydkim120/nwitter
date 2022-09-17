f0fdd83d39af8d826d5933f3c5c93583a43e2294
dam <yedam.kim@growthsoft.co.kr>
2022-09-17 13:47:40 +0900

    feat- 로그인, 계정생성 기본 로직 세팅

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AppRouter from "components/Router";
 import { authService } from 'fbase'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser) // 없으면 null
+  const [init, setInit] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false) // 로그인 유무
+
+  useEffect(() => { 
+    authService.onAuthStateChanged((user) => { // 계정에 변화가 있을 때 실행되는 메서드
+      setIsLoggedIn(!!user)
+      setInit(true)
+    })
+  }, [])
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initializing...'}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,63 @@
-import React from 'react'
+import { authService } from 'fbase'
+import React, { useState } from 'react'
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [newAccount, setNewAccount] = useState(true)
+  const [error, setError] = useState('')
+
+  const onChange = (event) => {
+    const { target: { name, value } } = event // event.target. ...
+    if (name === 'email') setEmail(value)
+    else if (name === 'password') setPassword(value)
+  }
+  const onSubmit = async(event) => {
+    event.preventDefault()
+    try {
+      let data
+      if (newAccount) data = await authService.createUserWithEmailAndPassword(
+          email, password
+        )
+      else data = await authService.signInWithEmailAndPassword(email, password)
+      console.log('submit!', data)
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+  const toggleAccount = () => setNewAccount(prev => !prev)
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value={ newAccount ? 'Create Account' : 'Sign In' }
+        />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? 'Sign in' : 'Create Account'}
+      </span>
+      <div>
+        <button>Continue with Google</button>
+      </div>
+    </div>
+  )
+}
 export default Auth
