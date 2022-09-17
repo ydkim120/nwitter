3c54b625feeecb052816230d0088146b56003589
dam <yedam.kim@growthsoft.co.kr>
2022-09-17 14:17:18 +0900

    feat- Social 로그인(google) 기능 추가

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from 'fbase'
+import { authService, firebaseInstance } from 'fbase'
 import React, { useState } from 'react'
 
 const Auth = () => {
@@ -26,6 +26,21 @@ const Auth = () => {
     }
   }
   const toggleAccount = () => setNewAccount(prev => !prev)
+  const onSocialClick = async (event) => {
+    const {
+      target : { name },
+    } = event
+
+    let provider
+    if (name === 'google') {
+      provider = new firebaseInstance.auth.GoogleAuthProvider()
+    }
+    // else if (name === 'github') {
+      // provider = new firebaseInstance.auth.GithubAuthProvider()
+    // }
+    const data = await authService.signInWithPopup(provider)
+    console.log(data)
+  }
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -55,7 +70,12 @@ const Auth = () => {
         {newAccount ? 'Sign in' : 'Create Account'}
       </span>
       <div>
-        <button>Continue with Google</button>
+        <button 
+          name="google" 
+          onClick={onSocialClick}
+        >
+          Continue with Google
+        </button>
       </div>
     </div>
   )
